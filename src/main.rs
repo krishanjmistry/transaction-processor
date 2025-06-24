b126716dfd9ef3906dbe54f4f44b8b35ee6aa990
Krishan Mistry <krishan@krishanmistry.com>
2025-06-24 18:37:16 +0100

    Add types to represent CSV record and deserialize

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use csv::ReaderBuilder;
+use rust_decimal::Decimal;
+use serde::Deserialize;
 
 fn main() {
     if std::env::args().len() != 2 {
@@ -14,8 +16,39 @@ fn main() {
         .from_path(file_path)
         .expect("Failed to create CSV reader");
 
-    for record in rdr.records() {
-        let record = record.expect("Failed to read record");
+    for record in rdr.deserialize() {
+        let record: CsvRecord = record.expect("Failed to read record");
         println!("{:?}", record);
     }
 }
+
+// Unfortunately, we can't use internally tagged enums on CSVs
+// Hence, we can't directly deserialize into a type that better represents the fact that only a deposit and withdrawal require the amount field.
+// https://github.com/BurntSushi/rust-csv/pull/231
+#[derive(Debug, Deserialize)]
+struct CsvRecord {
+    #[serde(rename = "type")]
+    transaction_type: TransactionType,
+    client: ClientId,
+    #[serde(rename = "tx")]
+    transaction: TransactionId,
+    amount: Option<MonetaryAmount>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+#[derive(Debug, Deserialize)]
+struct ClientId(u16);
+
+#[derive(Debug, Deserialize)]
+struct TransactionId(u32);
+
+type MonetaryAmount = Decimal;
