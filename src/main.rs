8651673bb38e5f4243940c69df9fd1ee6058b552
Krishan Mistry <krishan@krishanmistry.com>
2025-06-24 18:32:52 +0100

    Add basic CSV file opening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use csv::ReaderBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().len() != 2 {
+        eprintln!("Usage: cargo run -- /path/to/file.csv");
+        std::process::exit(1);
+    }
+
+    let file_path = std::env::args().nth(1).expect("No file path provided");
+
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(true)
+        .trim(csv::Trim::All)
+        .from_path(file_path)
+        .expect("Failed to create CSV reader");
+
+    for record in rdr.records() {
+        let record = record.expect("Failed to read record");
+        println!("{:?}", record);
+    }
 }
